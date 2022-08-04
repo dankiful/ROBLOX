b10e1d60c3c7722a4dbd1b83c9ebc2fdca9a21cb
dankiful <63526770+dankiful@users.noreply.github.com>
2022-08-03 23:10:27 -0400

    Create userspoofer.lua
    This is a very simple Client-Sided Roblox UserID/UserName Spoofer. You should always use this when using a obfuscated script incase the developers of that script sell data such as usernames to admins of a game to get you banned.

---
+++ Open-Source/userspoofer.lua
@@ -0,0 +1,15 @@
+-- This is a Client-Sided Roblox UserID/UserName Spoofer. You should always use this when using a obfuscated script incase the developers of that script sell data such as usernames to admins of a game to get you banned.
+local LP = game.Players.LocalPlayer
+function spoof()
+game.Players.LocalPlayer.UserId = 1337
+game.Players.LocalPlayer.Name = "0sid0"
+game.Players.LocalPlayer.UserName "0sid0"
+end
+wait(1)
+
+spoof()
+
+
+Player.CharacterAdded:Connect(function()
+    spoof()
+end)
